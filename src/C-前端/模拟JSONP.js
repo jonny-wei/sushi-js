b968603f7eca4ffa84522cc502233e087562835b
Jonny Wei <18174005984@163.com>
2021-04-16 10:32:35 +0800

    :ambulance: 补充校正

--- src/C-前端/模拟JSONP.js
+++ src/C-前端/模拟JSONP.js
@@ -3,11 +3,12 @@
  *
  * script标签不遵循同源协议，可以用来进行跨域请求，优点就是兼容性好但仅限于GET请求
  * 
- * JSONP 是一种非正式传输协议，允许用户传递一个callback给服务端，
+ * JSONP 是一种非正式传输协议，允许用户传递一个callback的参数给服务端，
  * 然后服务端返回数据时会将这个callback 参数作为函数名来包裹住 JSON 数据，
  * 这样客户端就可以随意定制自己的函数来自动处理返回数据了。
  * 当GET请求从后台页面返回时，可以返回一段JavaScript代码，
  * 这段代码会自动执行，可以用来负责调用后台页面中的一个callback函数。
+ * jsonp 返回的是一个 Promise
  */
 const jsonp = ({ url, params, callbackName }) => {
   const generateUrl = () => {
--- src/C-前端/解析 URL 参数为对象.js
+++ src/C-前端/解析 URL 参数为对象.js
@@ -3,6 +3,7 @@
  * 
  */
 function parseParam(url) {
+  // /\?(.+)$/
   const paramsStr = /.+\?(.+)$/.exec(url)[1]; // 将 ? 后面的字符串取出来
   const paramsArr = paramsStr.split("&"); // 将字符串以 & 分割后存到数组中
   let paramsObj = {};
